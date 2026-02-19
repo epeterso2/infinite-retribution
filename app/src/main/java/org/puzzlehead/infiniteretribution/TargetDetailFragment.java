4c1322d055f1dba5aca390528505a0c53383dcf5
Eric <eric@puzzlehead.org>
2026-02-18 19:19:33 -0500

    Upgraded to latest Android

--- app/src/main/java/org/puzzlehead/infiniteretribution/TargetDetailFragment.java
+++ app/src/main/java/org/puzzlehead/infiniteretribution/TargetDetailFragment.java
@@ -1,8 +1,8 @@
 package org.puzzlehead.infiniteretribution;
 
 import android.os.Bundle;
-import android.support.design.widget.CollapsingToolbarLayout;
-import android.support.v4.app.Fragment;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import androidx.fragment.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
